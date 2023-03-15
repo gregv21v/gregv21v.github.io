306694e4afa9b847363b0d1ea62e062f0b285a0b
Gregory Venezia <gregv21v@users.noreply.github.com>
2023-03-15 08:36:00 -0400

    Update

--- deploy.bat
+++ deploy.bat
@@ -1,28 +1,27 @@
-
-# build
+rem build
 cd vue
 npm run build
 
-# navigate into the build output directory
+rem navigate into the build output directory
 cd ..
 
 
 
-# copy dist files to root overwriting existing files
+rem copy dist files to root overwriting existing files
 xcopy "vue/dist" . /E
 
 
-# if you are deploying to a custom domain
-# echo 'www.example.com' > CNAME
+rem if you are deploying to a custom domain
+rem echo 'www.example.com' > CNAME
 
 git init
 git add -A
 git commit -m 'deploy'
 
-# if you are deploying to https://<USERNAME>.github.io
+rem if you are deploying to https://<USERNAME>.github.io
 git push -f git@github.com:gregv21v/gregv21v.github.io.git master
 
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+rem if you are deploying to https://<USERNAME>.github.io/<REPO>
+rem git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd ..
