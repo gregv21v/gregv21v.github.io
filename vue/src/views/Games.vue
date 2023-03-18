7ce2d5281cd7e5407db2fed802fb8abfd280c74b
Gregory Venezia <gregv21v@users.noreply.github.com>
2023-03-18 10:53:43 -0400

    Update

--- vue/src/views/Games.vue
+++ vue/src/views/Games.vue
@@ -7,7 +7,7 @@
       description="a game where you start with a small farm and slowly expand it"
       imageName="compact-farmer"
       repoLink="https://github.com/gregv21v/Compact-Farmer"
-      page="https://gregv21v.github.io/Compact-Farmer/"
+      gameLink="http://gregv21v.github.io/Compact-Farmer/"
     />
 
     <Project
@@ -15,7 +15,7 @@
       description="a game about mixing Chemicals to create better chemicals"
       imageName="chemistry-lab"
       repoLink="https://github.com/gregv21v/Chem-Lab"
-      page="https://gregv21v.github.io/Chem-Lab/"
+      gameLink="http://gregv21v.github.io/Chem-Lab/"
     />
 
   </div>
