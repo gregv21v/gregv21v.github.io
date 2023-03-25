8b7dcb02452e5f074a7827dde168f9b8c1337593
Gregory Venezia <gregv21v@users.noreply.github.com>
2023-03-25 19:37:21 -0400

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
 import solid from "solid-start/vite";
+import solidStatic from "solid-start-static";
 import { defineConfig } from "vite";
 export default defineConfig({
+  base: "./",
   plugins: [
     solid({
       ssr: false,
-      outDir: "dist",
-      base: "./"
+      adapter: solidStatic()
     }),
   ],
 });
