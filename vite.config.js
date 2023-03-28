b155b6d4333bfeffd95082ba70126067b4b9d92c
Gregory Venezia <gregv21v@users.noreply.github.com>
2023-03-27 21:53:03 -0400

    updated config, and static start

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,14 @@
 import solid from "solid-start/vite";
-//import solidStatic from "solid-start-static";
-import netlify from "solid-start-netlify";
+import solidStatic from "solid-start-static";
+//import netlify from "solid-start-netlify";
 import { defineConfig } from "vite";
 export default defineConfig({
+  build: {
+    outDir: "dist"
+  },
   plugins: [
     solid({
-      adapter: netlify()
+      adapter: solidStatic()
     }),
   ],
 });
